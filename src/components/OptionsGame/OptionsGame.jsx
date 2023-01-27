cd9aa65b44f8172ca1ec81f64be025433c4233a7
Bruno <brunoowap@gmail.com>
2023-01-27 13:45:30 -0300

    adicionando opção de escolher papel

--- src/components/OptionsGame/OptionsGame.jsx
+++ src/components/OptionsGame/OptionsGame.jsx
@@ -1,6 +1,9 @@
 import "./options.scss";
 import GameCard from "../GameCard/GameCard";
 import Scissors from "../../assets/icon-scissors.svg";
+import Rock from "../../assets/icon-rock.svg";
+import Paper from "../../assets/icon-paper.svg";
+
 export default function OptionsGame(props) {
   return (
     <div className="backgroundGame">
@@ -8,6 +11,7 @@ export default function OptionsGame(props) {
         borderColor="containerIcon--BorderScissor"
         iconCard={Scissors}
       />
+      <GameCard borderColor="containerIcon--BorderPaper" iconCard={Paper} />
     </div>
   );
 }
