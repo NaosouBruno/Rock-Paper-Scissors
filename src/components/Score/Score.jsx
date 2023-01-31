e7ba27ab54ea9e1e04c3e2626ed9ce6305b14109
Bruno <brunoowap@gmail.com>
2023-01-31 15:03:03 -0300

    corrigindo erros no scss do gamecard

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -3,8 +3,15 @@ import "./score.scss";
 export default function Score() {
   return (
     <div className="score">
-      <div></div>
-      <div></div>
+      <div className="score--describ">
+        <span>Rock</span>
+        <span>Paper</span>
+        <span>Scissors</span>
+      </div>
+      <div className="score--spots">
+        <span>Score</span>
+        <span>12</span>
+      </div>
     </div>
   );
 }
