4816051f8a6b992471a872f7b26152aa43c20b22
Bruno <brunoowap@gmail.com>
2023-02-02 11:42:18 -0300

    adicionando scss da tela de confronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Game from "./components/pages/Game";
-/* import Home from "./components/pages/Home"; */
+import Home from "./components/pages/Home";
 
 function App() {
   return (
--- src/components/pages/Game.jsx
+++ src/components/pages/Game.jsx
@@ -1,3 +1,21 @@
-export default function Game() {
-  return <div>Game</div>;
+import GameCard from "../GameCard/GameCard";
+import Score from "../Score/Score";
+import "../../styles/game.scss";
+export default function Game({ playerChoice }) {
+  /* receber o conteudo do player */
+  return (
+    <>
+      <Score />
+      <div className="game">
+        <div className="game--Picked">
+          <GameCard />
+          You Picked
+        </div>
+        <div>
+          <GameCard />
+          The House Picked
+        </div>
+      </div>
+    </>
+  );
 }
