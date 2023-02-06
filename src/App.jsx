5b147671734ed6d7158b1d4895961cf61c3131ee
Bruno <brunoowap@gmail.com>
2023-02-06 14:27:50 -0300

    iniciando logica para o useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import { useState } from "react";
 import Game from "./components/pages/Game";
 import Home from "./components/pages/Home";
 
 function App() {
-  return (
-    <div className="main">
-      <Game />
-    </div>
-  );
+  const [letsPlay, setLetsPlay] = useState(false);
+  return <div className="main">{letsPlay ? <Game /> : <Home />}</div>;
 }
 
 export default App;
